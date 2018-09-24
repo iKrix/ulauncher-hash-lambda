f27cd49c4ba2cfa44df50619382a8bf5f87d2be9
xtrcode <webmaster@xtracode.ws>
2018-09-24 11:00:02 +0200

    fix rebase

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 
+
 class Hash2Extension(Extension):
     def __init__(self):
         super(Hash2Extension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
 
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
@@ -23,13 +25,23 @@ class KeywordQueryEventListener(EventListener):
                 keywordId = kwId
 
         # Show the algorithm specified as keyword, or all if the keyword was "hash"
-        algos = hashlib.algorithms if keywordId == 'hash' else [keywordId]
+        algos = hashlib.algorithms_available if keywordId == 'hash' else [keywordId]
 
-        for algo in algos:
-            hash = getattr(hashlib, algo)(argument).hexdigest()
-            items.append(ExtensionResultItem(icon='icon.png', name=hash, description=algo, on_enter=CopyToClipboardAction(hash), highlightable=False))
+        # credits: https://stackoverflow.com/a/48283398
+        marker = set()
+        unique = [not marker.add(x.lower()) and x for x in algos if x.lower() not in marker]
+        unique.sort()
+
+        for algo in unique:
+            h = hashlib.new(algo)
+            h.update(argument)
+            hash = h.hexdigest()
+            items.append(
+                ExtensionResultItem(icon='icon.png', name=hash, description=algo.upper(), on_enter=CopyToClipboardAction(hash),
+                                    highlightable=True))
 
         return RenderResultListAction(items)
+
 
 if __name__ == '__main__':
     Hash2Extension().run()
